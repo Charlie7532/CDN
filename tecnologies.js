4048ead3aa001688c89f08b30b60b489a2e133fa
CHARLIE <jcarlosbotero1@outlook.com>
2023-11-23 21:33:40 -0500

    Update tecnologies.js

--- tecnologies.js
+++ tecnologies.js
@@ -1,4 +1,5 @@
 function displayTechnologies(projectTechnologies) {
+	console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 	// Fetch technologies from JSON file
 	fetch('https://raw.githubusercontent.com/Charlie7532/CDN/main/technologies.json')
 		.then((response) => response.json())
