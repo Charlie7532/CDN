1b16eca4f31c8cf4507e6d6def4cde8f56da1564
CHARLIE <jcarlosbotero1@outlook.com>
2023-12-02 17:39:56 -0500

    Update technologies.js

--- assets/js/technologies.js
+++ assets/js/technologies.js
@@ -24,3 +24,28 @@ function displayTechnologies(projectTechnologies) {
 		})
 		.catch((error) => console.error('Error fetching technologies:', error));
 }
+
+function displayTechnologieswithID(projectTechnologies,containerID) {
+	// Fetch technologies from JSON file
+	fetch('https://cdn.juancbotero.com/assets/js/technologies.json')
+		.then((response) => response.json())
+		.then((technologies) => {
+			// Filter technologies based on projectTechnologies
+			const technologies_used = technologies.filter((tech) => projectTechnologies.includes(tech.id));
+
+			// Display technologies in the HTML
+			const technologyContainer = document.getElementById(containerID);
+			technologyContainer.innerHTML = ''; // Clear existing content
+
+			technologies_used.forEach((tech) => {
+				const col = document.createElement('div');
+				col.className = 'col-4 col-md-3 mb-4 text-center';
+				col.innerHTML = `
+                    <img src="${tech.logo}" alt="${tech.name} Icon" class="technology-icon mx-auto d-block" style="width: 50px; height: 50px;"> 
+                    <p>${tech.name}</p>
+                    `;
+				technologyContainer.appendChild(col);
+			});
+		})
+		.catch((error) => console.error('Error fetching technologies:', error));
+}
